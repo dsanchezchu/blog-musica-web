e396b587726e2266ef93da0d317f2c4a188e7cde
dsanchezchu <165108758+dsanchezchu@users.noreply.github.com>
2025-01-12 00:19:40 -0500

    Migracion a Angular 19, Componentes Nav, Login, Footer, Foro Creados

---
+++ blog-rock-latam-web/src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ForoComponent } from './components/foro/foro.component';
+import { CollaborateComponent } from './components/collaborate/collaborate.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+export const routes: Routes = [
+    {
+        path: '', 
+        redirectTo:'home',
+        pathMatch:'full'
+    },
+    {
+        path: 'home', component:HomeComponent
+    },
+    {
+        path: 'foro', component:ForoComponent
+    },
+    {
+        path: 'collaborate', component:CollaborateComponent
+    },
+    {
+        path: 'login', component:LoginComponent
+    }
+];
---
+++ blog-rock-latam-web/src/app/components/foro/foro.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-foro',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './foro.component.html',
+  styleUrl: './foro.component.scss'
+})
+export class ForoComponent {
+
+}
